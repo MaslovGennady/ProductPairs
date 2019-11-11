9a39412471c3ccc067f71874631aea7f2f24fb02
MaslovGennady <maslovgennady@mail.ru>
2019-11-12 01:35:28 +0300

    Query beautifying

--- GetProductPairs.py
+++ GetProductPairs.py
@@ -9,7 +9,16 @@ with psycopg2.connect(dbname=data['dbname'], user=data['user'],
     
     with conn.cursor() as cursor:
         cursor.execute(
-            'select plu1, plu2, count(1) cnt from ( select distinct s1.id, case when s1.plu >= s2.plu then s1.plu else s2.plu end plu1, case when s1.plu >= s2.plu then s2.plu else s1.plu end plu2 from sales s1 join sales s2 on s1.id = s2.id where s1.plu <> s2.plu) t group by plu1, plu2 order by count(1) desc'
+            'select plu1, plu2, count(1) cnt '
+               'from ( '
+                      'select distinct s1.id, '
+                             'case when s1.plu >= s2.plu then s1.plu else s2.plu end plu1, '
+                             'case when s1.plu >= s2.plu then s2.plu else s1.plu end plu2 '
+                        'from sales s1 '
+                             'join sales s2 on s1.id = s2.id where s1.plu <> s2.plu '
+                    ') t '
+              'group by plu1, plu2 '
+              'order by count(1) desc '
         )
 
         print('Query Executed!')
